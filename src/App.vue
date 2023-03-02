c172127218c296994ea04e0976facb6be31397ad
Hasan <hasanizmirr@gmail.com>
2023-03-02 15:06:13 +0300

    improvement: Routes updated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/product-list">Products</router-link>
+    <router-link to="/cart">Cart</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-@import './assets/styles/style';
+@import "./assets/styles/style";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import ProductList from "../views/ProductList.vue";
+import CartView from "../views/CartView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    redirect: () => {
+      return { path: "/product-list" };
+    },
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/product-list",
+    name: "ProductList",
+    component: ProductList,
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: CartView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
