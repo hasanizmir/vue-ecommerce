835e72da761edc685306c7832e9806a5d78bcdaf
Hasan <hasanizmirr@gmail.com>
2023-03-02 15:45:41 +0300

    feat: ProductListItem component created

---
+++ src/components/ProductListItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="product">
+    <img :src="item.image" alt="product" width="200" />
+    <p>{{ item.name }}</p>
+    <div class="product__body">
+      <span>{{ item.price }} {{ item.currency }}</span>
+      <button class="product__button">ADD BASKET</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.product {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid var(--border-color);
+  padding: var(--padding-md);
+
+  @media screen and (min-width: 460px) {
+    width: calc(100% / 2);
+
+    &:nth-child(2n) {
+      border-left: 0;
+    }
+  }
+
+  img,
+  p {
+    padding-bottom: var(--padding-md);
+  }
+
+  &__body {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  &__button {
+    background-color: var(--color-primary);
+    color: var(--color-white);
+    font-weight: var(--font-weight-semibold);
+    padding: var(--padding-sm) var(--padding-lg);
+    border: 2px solid var(--color-primary);
+    border-radius: var(--padding-xs);
+    transition: background-color 0.3s ease-in;
+    cursor: pointer;
+
+    &:hover {
+      background-color: transparent;
+      border: 2px solid var(--color-primary);
+      color: var(--color-primary);
+    }
+  }
+}
+</style>
