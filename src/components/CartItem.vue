f9064df644ce5d2130c4b573f4b75c7b7489a4cd
Hasan <hasanizmirr@gmail.com>
2023-03-05 17:39:08 +0300

    improvement: CartItem component updated

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,33 +1,64 @@
 <template>
   <div class="cart-item">
     <div class="cart-item__detail">
-      <img :src="data.image" :alt="data.name" width="150" height="150" />
+      <img :src="data.image" :alt="data.name" width="100" height="100" />
       <div class="cart-item__content">
         <div class="cart-item__name">{{ data.name }}</div>
         <div class="cart-item__price">
-          {{ data.price + " " + data.currency }}
+          {{ (data.price * amount).toFixed(2) + " " + data.currency }}
         </div>
       </div>
     </div>
     <div class="cart-item__footer">
       <div class="cart-item__qty">
-        <button class="cart-item__button">-</button>
-        <input class="cart-item__input" value="1"/>
-        <button class="cart-item__button">+</button>
+        <button
+          class="cart-item__button"
+          @click="decrease(data)"
+          :disabled="amount === 1"
+        >
+          -
+        </button>
+        <input class="cart-item__input" v-model="amount" />
+        <button class="cart-item__button" @click="increase(data.id)">+</button>
       </div>
-      <span class="cart-item__remove">REMOVE</span>
+      <span class="cart-item__remove"  @click="removeFromCart()">REMOVE</span>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: "CartItem",
+  data() {
+    return {
+      amount: 1,
+    };
+  },
   props: {
     data: {
       type: Object,
       required: true,
     },
+  },
+  methods: {
+    ...mapActions({
+      incrementItem: 'cart/incrementItemQuantity',
+      decrementItem: 'cart/decrementItemQuantity',
+      removeItem: 'cart/removeItem',
+    }),
+    increase() {
+      this.amount += 1;
+      this.incrementItem(this.data.id);
+    },
+    decrease() {
+      this.amount -= 1;
+      this.decrementItem(this.data);
+    },
+  },
+  created() {
+    this.amount = this.data.amount;
   },
 };
 </script>
@@ -44,7 +75,7 @@ export default {
   &__content {
     display: flex;
     gap: var(--space-xl);
-    margin: var(--space-xs) var(--space-sm);
+    margin: var(--space-xs) var(--space-md);
     flex-direction: column;
   }
 
@@ -64,7 +95,7 @@ export default {
   }
 
   &__qty {
-    width: 150px;
+    width: 100px;
     display: flex;
     justify-content: space-evenly;
   }
@@ -82,15 +113,16 @@ export default {
   }
 
   &__input {
-    width: 50px;
+    width: 30px;
     text-align: center;
     border: 1px solid #ccc;
   }
 
   &__remove {
     color: var(--color-black);
+    font-size: var(--font-size-sm);
     font-weight: var(--font-weight-semibold);
-    margin-right: var(--space-3xl);
+    margin-right: 30%;
     cursor: pointer;
   }
 }
